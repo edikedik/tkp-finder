2115cd1914c3ba5395b12c741b83c3096bcbf2e3
Ivan Reveguk <edikedikedikedik@gmail.com>
2022-11-20 13:58:04 +0100

    fix output parsing

--- tkp_finder/deeptm.py
+++ tkp_finder/deeptm.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 import biolib
-from lXtractor.core import ChainList, ChainSequence
+from lXtractor.core import ChainSequence
 from lXtractor.core.segment import Segment
 from lXtractor.util.seq import write_fasta
-from more_itertools import peekable, split_at
+from more_itertools import peekable
 
 
 class DeepTMHMM:
@@ -15,8 +15,8 @@ class DeepTMHMM:
         self.interface = biolib.load('DTU/DeepTMHMM')
 
     def run(
-            self, seqs: ChainList | abc.Iterable[ChainSequence] | abc.Iterable[tuple[str, str]] | Path
-    ) ->  list[tuple[str, list[Segment]]]:
+            self, seqs: abc.Iterable[ChainSequence] | abc.Iterable[tuple[str, str]] | Path
+    ) -> list[tuple[str, list[Segment]]]:
         match seqs:
             case abc.Iterable():
                 peek = peekable(seqs)
@@ -42,29 +42,34 @@ class DeepTMHMM:
             case _:
                 raise TypeError(f'Invalid input type {type(seqs)}')
 
-        return list(self.parse_output_gff(res.get_output_file('/TMRs.gff3')))
+        return list(self.parse_output_gff(res.get_output_file('/TMRs.gff3').get_data()))
 
     def run_cli(self, inp_fasta: Path | str):
         return self.interface.cli(args=f'--fasta {inp_fasta}')
 
     @staticmethod
     def parse_output_gff(inp: Path | str | bytes) -> abc.Iterator[tuple[str, list[Segment]]]:
-        def parse_chunk(xs: abc.Iterable[str]) -> tuple[str, list[Segment]]:
-            xs = map(lambda x: x.split('\t'), xs)
-            peek = peekable(xs)
-            first = peek.peek(None)
-            if first is None:
-                raise ValueError('Empty GFF3 output')
-            chain_id = first[0]
-            return chain_id, [Segment(int(x[2]), int(x[3]), x[1]) for x in xs]
+        def parse_chunk(c: str) -> tuple[str, list[Segment]]:
+            lines = map(
+                lambda x: x.split('\t'),
+                filterfalse(lambda x: not x or x.startswith('#'), c.split('\n')))
+            peek = peekable(lines)
+            fst = peek.peek(None)
+            assert fst, 'Non-empty chunk of data'
+            chain_id = fst[0]
+            return chain_id, [Segment(int(x[2]), int(x[3]), x[1]) for x in lines]
 
         if isinstance(inp, Path):
             inp = inp.read_text()
         if isinstance(inp, bytes):
             inp = inp.decode('utf-8')
 
-        lines = filterfalse(
-            lambda x: not x or x.startswith('#'), inp.split('\n'))
-
-        chunks = split_at(lines, lambda x: x.startswith('\\'))
+        chunks = inp.split('//')
         return map(parse_chunk, chunks)
+
+    def annotate(self, chains: abc.Iterable[ChainSequence]):
+        pass
+
+
+if __name__ == '__main__':
+    raise RuntimeError
